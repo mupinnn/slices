88ef5dfaf34f364993c7e7244996691ddf429351
Ahmad Muwaffaq <itsmupin@gmail.com>
2026-02-11 20:52:40 +0700

    feat: order confirmation dialog

--- slices/product-list-with-cart/src/components/cart/cart-card.tsx
+++ slices/product-list-with-cart/src/components/cart/cart-card.tsx
@@ -1,17 +1,32 @@
+import { useRef } from "react";
 import { useStore } from "@nanostores/react";
-import { $cart } from "@/stores";
+import { $cart, $showConfirmOrderDialog } from "@/stores";
 import IllustrationEmpty from "@/assets/assets/images/illustration-empty-cart.svg";
 import RemoveIcon from "@/assets/assets/images/icon-remove-item.svg";
 import CarboNeutralIcon from "@/assets/assets/images/icon-carbon-neutral.svg";
+import CheckIcon from "@/assets/assets/images/icon-order-confirmed.svg";
 import { formatCurrency } from "@/utils";
 import { Button } from "../ui/button";
 
 export function CartCard() {
   const cart = useStore($cart);
+  const orderConfirmationDialogRef = useRef<HTMLDialogElement>(null);
+
   const total = cart.reduce((acc, curr) => acc + curr.qty * curr.price, 0);
 
   function removeItem(name: string) {
     $cart.set(cart.filter(c => c.name !== name));
+  }
+
+  function confirmOrder() {
+    $showConfirmOrderDialog.set(true);
+    orderConfirmationDialogRef.current?.showModal();
+  }
+
+  function clearOrder() {
+    $cart.set([]);
+    $showConfirmOrderDialog.set(false);
+    orderConfirmationDialogRef.current?.close();
   }
 
   return (
@@ -57,9 +72,46 @@ export function CartCard() {
             </p>
           </div>
 
-          <Button className="mt-4">Confirm Order</Button>
+          <Button className="mt-4" onClick={confirmOrder}>
+            Confirm Order
+          </Button>
         </>
       )}
+
+      <dialog
+        ref={orderConfirmationDialogRef}
+        className="bottom-0 mt-auto max-w-[580px] rounded-t-[10px] backdrop:bg-black/35 sm:m-auto sm:w-[calc(100%-2rem)] sm:rounded-[10px]"
+      >
+        <div className="bg-white p-8">
+          <img src={CheckIcon.src} alt="" />
+          <div className="mt-6 flex flex-col gap-1">
+            <p className="text-4xl font-bold text-rose-900">Order Confirmed</p>
+            <p className="text-rose-500">We hope you enjoy your food!</p>
+          </div>
+          <div className="mt-6 flex flex-col bg-rose-50 px-6 py-2">
+            {cart.map(c => (
+              <div className="flex items-center justify-between gap-1 border-b border-rose-100 py-4">
+                <div className="flex flex-col">
+                  <p className="text-sm font-semibold text-rose-900">{c.name}</p>
+                  <div className="flex items-center gap-2 text-sm">
+                    <span className="text-red font-semibold">{c.qty}x</span>
+                    <span className="ml-4 text-rose-500">@ {formatCurrency(c.price)}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+
+            <div className="mt-6 flex items-center justify-between pb-4">
+              <p className="text-sm font-semibold text-rose-500">Order Total</p>
+              <p className="text-2xl font-bold">{formatCurrency(total)}</p>
+            </div>
+          </div>
+
+          <Button className="mt-6 w-full" onClick={clearOrder}>
+            Start New Order
+          </Button>
+        </div>
+      </dialog>
     </section>
   );
 }
