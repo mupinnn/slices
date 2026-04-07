97dd10c8fde7bcde20468a7dccc49aeaa00a9850
Ahmad Muwaffaq <itsmupin@gmail.com>
2026-04-07 18:23:07 +0700

    feat(advice-generator-app): mobile design

---
+++ slices/advice-generator-app/README.md
@@ -0,0 +1,8 @@
+# Advice Generator App
+
+You can put screenshot here
+
+Some description here
+
+- Live site: https://mupinnn.github.io/slices/advice-generator-app
+- Challenge/design link:
---
+++ slices/advice-generator-app/astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://mupinnn.github.io",
+  base: "/slices/advice-generator-app",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [icon()],
+});
